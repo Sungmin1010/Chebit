4a3e275d9a9c1b68569420a160a1104dfd443a4f
sungmin <lmsgsm1@gmail.com>
2021-08-03 02:45:00 +0900

    [add] 주간 습관 조회 화면 추가

--- src/main/java/com/nesty/chebit/web/RecordController.java
+++ src/main/java/com/nesty/chebit/web/RecordController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +24,11 @@ public class RecordController {
         log.info("----POST /chebit/record [기록 추가]-----");
         recordAddDto.convertToLocalDate();
         return recordService.addTodayRecord(recordAddDto.getHabitId(), recordAddDto.getRecDate());
+    }
+
+    @GetMapping("/chebit/weekly")
+    public String getWeekly(){
+        log.info("----GET /chebit/monthly [위클리 화면]-----");
+        return "weekly/weeklyHabit";
     }
 }
